bd7885faa285eb0a7b9a36b16b6d230d4b37c78a
Damian <Damian@DRBuero>
2019-09-09 21:19:28 +0200

    Reorg Sources

---
+++ CRClanStats/vendor/firegore2/clash-royale-php/src/Objects/ConstantsObjects/Challenge.php
@@ -0,0 +1,62 @@
+<?php
+    /*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ ~                                                                                                                                                                                                                                                          ~
+ ~ Copyright (c) 2018 by firegore (https://firegore.es) (git:firegore2)                                                                                                                                                                                     ~
+ ~ This file is part of clash-royale-php.                                                                                                                                                                                                                   ~
+ ~                                                                                                                                                                                                                                                          ~
+ ~ clash-royale-php is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+ ~ clash-royale-php is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.                                                                  ~
+ ~ See the GNU Affero General Public License for more details.                                                                                                                                                                                              ~
+ ~ You should have received a copy of the GNU General Public License along with clash-royale-php.                                                                                                                                                           ~
+ ~ If not, see <http://www.gnu.org/licenses/> 2018.05.31                                                                                                                                                                                                    ~
+ ~                                                                                                                                                                                                                                                          ~
+ ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+
+    namespace CR\Objects\ConstantsObjects;
+    use CR\Objects\BaseObject;
+
+
+    /**
+     *  Challenge object
+     * @method     getName()
+     * @method     getGameMode()
+     * @method     getEnabled()
+     * @method     getJoinCost()
+     * @method     getJoinCostResource()
+     * @method     getMaxWins()
+     * @method     getMaxLoss()
+     * @method     getRewardCards()
+     * @method     getRewardGold()
+     * @method     getRewardSpell()
+     * @method     getRewardSpellMaxCount()
+     * @method     getNameEn()
+     * @method     getKey()
+     * @method     getId()
+     *
+     */
+
+
+    class Challenge extends BaseObject
+    {
+
+
+        /**
+         * {@inheritdoc}
+         */
+        public function primaryKey()
+        {
+            return "id";
+        }
+
+
+        /**
+         * {@inheritdoc}
+         */
+        public function relations()
+        {
+            return [
+            ];
+        }
+
+
+    }
